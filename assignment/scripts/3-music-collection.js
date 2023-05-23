0e5f4368e43f5edb8d5fef90dc43e51bfcb6d487
Georgio Harris <georgie.to.harris@gmail.com>
2023-05-23 15:28:41 -0500

    Completed first task in js file for assignment.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,18 @@
 console.log('***** Music Collection *****')
+let collection = [];
+
+function addToCollection (title, artist, yearPublished){
+    console.log("You are now running addToCollection.");
+    let album = {
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished
+    }
+    collection.push(album);
+    return album;
+}
+
+console.log(addToCollection("Rock Saviors", "Generic Rock Band", 2008));
+console.log(collection);
+console.log(addToCollection("Scissors", "Generic Rap Band", 2006));
+console.log(collection[1]);
