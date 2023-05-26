298777b07912a17590277d8c881d1993cdde0acd
Georgio Harris <georgie.to.harris@gmail.com>
2023-05-25 23:51:02 -0500

    In the process of building search function for collection that earches for albums. Going through building and iterative stage.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -8,27 +8,47 @@ let collection = [];
 // - FUNCTIONS -
 // Function to add an album to the collection
 function addToCollection (title, artist, yearPublished){
-    console.log("You are now running addToCollection.");
+    console.log("Running addToCollection.");
     let album = {
         title: title,
         artist: artist,
         yearPublished: yearPublished
     } // end declaration of album object literal with input parameters
     collection.push(album);
-    return album;
+    console.log("Album added:", album);
+    return true;
 } // end addToCollection. Adding object to collection array.
 
-function findArtist(collectionToAdd){
+function findByArtist(artist){
+console.log("Running findByArtist.");
+    for(i = 0; i < collection.length; i ++){
+        if (collection[i].artist === artist) {
+            console.log(`The artist "${artist}" is in this collection!`);
+            return true;
+        } 
+    }
+    console.log(`The artist ${artist} is unfortunately, not in this collection.`);
+    return false;
+} // end conditional, artist searched
 
+// Function to search for album in a collection and output artist and year published
+function search (albumToSearch){
+console.log("Actively running search.")
+for(let album of collection)
+if(album === albumToSearch){
+console.log("Yes!");
+return true;
+}
 }
 
 // Function to show albums in a collection
 function showCollection(collectionToShow){
+    let albumString = [];
     console.log("Within this collection, there are", collectionToShow.length, "albums.");
     for(i = 0; i < collectionToShow.length; i ++){
-        collectionToShow[i] = `${collectionToShow[i].title} by ${collectionToShow[i].artist}, published in ${collectionToShow[i].yearPublished}`
+    albumString.push(`${collectionToShow[i].title} by ${collectionToShow[i].artist}, published in ${collectionToShow[i].yearPublished}`); 
     } // end show title, artist, publish date
-    console.log(collectionToShow);
+    console.log(albumString);
     return true;
 } // end showCollection function
 // - END FUNCTIONS -
@@ -41,7 +61,7 @@ console.log(addToCollection("Sugar Rush", "Pewee Babies", 2025));
 console.log(addToCollection("Brainwashed in Zombie Juice", "Apocalypse Rockers", 2132));
 console.log(addToCollection("Not So Curable", "Apocalypse Rockers", 2165));
 console.log(addToCollection("Fairy Tales", "Pewee Babies", 2028));
-console.log(addToCollection("Hold the Artichoke", "We Fish Flounders", 2047));
+console.log(addToCollection("Hold the Anchovies", "We Fish Flounders", 2047));
 console.log(addToCollection("Lost in the Backrooms", "Liminal Space", 2021));
 console.log(collection);
 console.log("*** - END addToCollection FUNCTION TESTING - ***");
@@ -50,4 +70,19 @@ console.log("*** - END addToCollection FUNCTION TESTING - ***");
 // - showCollection Function Testing -
 console.log("*** - showCollection FUNCTION TESTING - ***");
 console.log(showCollection(collection));
+console.log(collection);
 // - End showCollection Function Testing -
+
+// - findByArtist Function Testing -
+console.log("*** - findByArtist FUNCTION TESTING - ***");
+console.log(findByArtist("Pewee Babies"));
+console.log(findByArtist("We Fish Flounders"));
+console.log(findByArtist("Liminal Space"));
+console.log(findByArtist("Nine Lives"));
+console.log(collection);
+// - End findByArtist Function Testing -
+
+// - search Function Testing -
+console.log("*** - search FUNCTION TESTING - ***");
+console.log(seach("Fairy Tales"));
+// - End search Function Testing -
